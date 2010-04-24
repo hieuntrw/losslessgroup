b314629cd6c55de820a8fc1fa1057d8424126d6c
tranchihieu <tranchihieu@fb5753d4-eb16-e1d5-651e-f0772e5c7e89>
2010-04-24 17:06:42 +0000

    create entity

---
+++ LabourManagementSystem/src/entity/Worker.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+/**
+ *
+ * @author Noi Nho
+ */
+public class Worker {
+
+}
