1f167bcf0ab9ccba34c74c71cdebe57a9519d9fd
tranchihieu <tranchihieu@fb5753d4-eb16-e1d5-651e-f0772e5c7e89>
2010-04-27 11:20:07 +0000

    create entity

--- LabourManagementSystem/src/entity/Account.java
+++ LabourManagementSystem/src/entity/Account.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package entity;
 
 /**
@@ -10,5 +9,103 @@ package entity;
  * @author Noi Nho
  */
 public class Account {
+    private int accountID;
+    private String username;
+    private String password;
+    private int roleID;
+    private boolean status;
+    private String lassError;
 
+    /*
+     * Constructor
+     */
+
+    public Account() {
+        setLassError("");
+    }
+
+
+    /**
+     * @return the accountID
+     */
+    public int getAccountID() {
+        return accountID;
+    }
+
+    /**
+     * @param accountID the accountID to set
+     */
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the roleID
+     */
+    public int getRoleID() {
+        return roleID;
+    }
+
+    /**
+     * @param roleID the roleID to set
+     */
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    /**
+     * @return the status
+     */
+    public boolean isStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the lassError
+     */
+    public String getLassError() {
+        return lassError;
+    }
+
+    /**
+     * @param lassError the lassError to set
+     */
+    public void setLassError(String lassError) {
+        this.lassError = lassError;
+    }
 }
