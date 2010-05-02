8f743d0cc5b928510fbab3fd5f9c67fd185cfdf3
cp200809l.thuan <cp200809l.thuan@fb5753d4-eb16-e1d5-651e-f0772e5c7e89>
2010-05-02 04:43:25 +0000

    update package util + dao

---
+++ LabourManagementSystem/src/com/aptech/labourmanagement/util/PassEncryption.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.aptech.labourmanagement.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.misc.BASE64Encoder;
+
+/**
+ *
+ * @author JONNY
+ */
+public class PassEncryption {
+
+    public String encryptPass(String pass) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] b = md.digest(pass.getBytes());
+            BASE64Encoder base = new BASE64Encoder();
+            return base.encode(b);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(PassEncryption.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
