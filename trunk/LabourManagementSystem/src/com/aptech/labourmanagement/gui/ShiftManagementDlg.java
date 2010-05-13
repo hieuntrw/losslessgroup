fe5af3a1fe4cc6ae344759a7fc5f6ac9b4f20caa
tranchihieu <tranchihieu@fb5753d4-eb16-e1d5-651e-f0772e5c7e89>
2010-05-13 20:50:43 +0000

    connect database

--- trunk/LabourManagementSystem/src/com/aptech/labourmanagement/gui/ShiftManagementDlg.java
+++ trunk/LabourManagementSystem/src/com/aptech/labourmanagement/gui/ShiftManagementDlg.java
@@ -8,18 +8,37 @@
  *
  * Created on May 10, 2010, 5:38:34 PM
  */
-
 package com.aptech.labourmanagement.gui;
 
+import com.aptech.labourmanagement.component.ColumnData;
 import com.aptech.labourmanagement.component.LookAndFeel;
+import com.aptech.labourmanagement.component.ObjectTableModel;
+import com.aptech.labourmanagement.entity.Shift;
+import com.aptech.labourmanagement.services.ShiftServices;
 import java.awt.Toolkit;
+import java.util.ArrayList;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.JViewport;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
 
 /**
  *
  * @author lab
  */
 public class ShiftManagementDlg extends javax.swing.JDialog {
+
+    public ShiftServices shiftSer;
+    public ObjectTableModel tableModel;
+    public ArrayList<Shift> arrShift = new ArrayList<Shift>();
+    //contains information header of columns
+    public JTable headerTable;
+    //index selected in table
+    int index = -1;
+    int selection;
 
     /** Creates new form ShiftManagementDlg */
     public ShiftManagementDlg(java.awt.Frame parent, boolean modal) {
@@ -34,6 +53,8 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
         this.setBounds((screenWidth - width) / 2, (screenHeight - heigh) / 2, width, heigh);
         new LookAndFeel(this);
         setIconImage(new ImageIcon(getClass().getResource("../icon/LMSIcon.png")).getImage());
+        loadDataOnTable();
+        disableFields();
     }
 
     /** This method is called from within the constructor to
@@ -47,8 +68,8 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
         java.awt.GridBagConstraints gridBagConstraints;
 
         jPanel1 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        scrShift = new javax.swing.JScrollPane();
+        tblShift = new javax.swing.JTable();
         jPanel3 = new javax.swing.JPanel();
         btnAdd = new javax.swing.JButton();
         btnEdit = new javax.swing.JButton();
@@ -77,9 +98,9 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Shift list", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 11), new java.awt.Color(0, 0, 0))); // NOI18N
         jPanel1.setLayout(new java.awt.BorderLayout(0, 10));
 
-        jScrollPane1.setPreferredSize(new java.awt.Dimension(400, 200));
+        scrShift.setPreferredSize(new java.awt.Dimension(400, 200));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tblShift.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -87,22 +108,42 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
                 "No.", "Shift name", "Time in", "Time out"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        tblShift.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblShiftMouseClicked(evt);
+            }
+        });
+        scrShift.setViewportView(tblShift);
 
-        jPanel1.add(jScrollPane1, java.awt.BorderLayout.CENTER);
+        jPanel1.add(scrShift, java.awt.BorderLayout.CENTER);
 
         jPanel3.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
 
         btnAdd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/aptech/labourmanagement/icon/add.png"))); // NOI18N
         btnAdd.setText("Add");
+        btnAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
         jPanel3.add(btnAdd);
 
         btnEdit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/aptech/labourmanagement/icon/edit.png"))); // NOI18N
         btnEdit.setText("Edit");
+        btnEdit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditActionPerformed(evt);
+            }
+        });
         jPanel3.add(btnEdit);
 
         btnDelete.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/aptech/labourmanagement/icon/delete2.png"))); // NOI18N
         btnDelete.setText("Delete");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
         jPanel3.add(btnDelete);
 
         jPanel1.add(jPanel3, java.awt.BorderLayout.SOUTH);
@@ -113,6 +154,7 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         gridBagConstraints.weightx = 0.6;
+        gridBagConstraints.weighty = 0.85;
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         getContentPane().add(jPanel1, gridBagConstraints);
 
@@ -152,24 +194,24 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         jPanel4.add(jLabel4, gridBagConstraints);
 
-        cbbTimeInH.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbbTimeInH.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23" }));
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.weightx = 0.3;
-        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 0);
         jPanel4.add(cbbTimeInH, gridBagConstraints);
 
-        cbbTimeInM.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbbTimeInM.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55" }));
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 3;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.weightx = 0.3;
-        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 5);
         jPanel4.add(cbbTimeInM, gridBagConstraints);
 
         txtShiftName.setColumns(20);
@@ -183,24 +225,24 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         jPanel4.add(txtShiftName, gridBagConstraints);
 
-        cbbTimeOutH.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbbTimeOutH.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23" }));
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.weightx = 0.3;
-        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 0);
         jPanel4.add(cbbTimeOutH, gridBagConstraints);
 
-        cbbTimeOutM.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbbTimeOutM.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55" }));
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 3;
         gridBagConstraints.gridy = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.weightx = 0.3;
-        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 5);
         jPanel4.add(cbbTimeOutM, gridBagConstraints);
 
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -227,10 +269,20 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
 
         btnSave.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/aptech/labourmanagement/icon/check.png"))); // NOI18N
         btnSave.setText("Save");
+        btnSave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSaveActionPerformed(evt);
+            }
+        });
         jPanel2.add(btnSave);
 
         btnCancel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/aptech/labourmanagement/icon/delete.png"))); // NOI18N
         btnCancel.setText("Cancel");
+        btnCancel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelActionPerformed(evt);
+            }
+        });
         jPanel2.add(btnCancel);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
@@ -238,7 +290,7 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
         gridBagConstraints.gridy = 3;
         gridBagConstraints.gridwidth = 4;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        gridBagConstraints.insets = new java.awt.Insets(15, 5, 5, 5);
         jPanel4.add(jPanel2, gridBagConstraints);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
@@ -247,6 +299,7 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         gridBagConstraints.weightx = 0.4;
+        gridBagConstraints.weighty = 0.85;
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         getContentPane().add(jPanel4, gridBagConstraints);
 
@@ -266,20 +319,195 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridwidth = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 0.15;
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 20, 5);
         getContentPane().add(jPanel5, gridBagConstraints);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void tblShiftMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblShiftMouseClicked
+        // TODO add your handling code here:
+        index = tblShift.getSelectedRow();
+        if (index > -1) {
+            btnDelete.setEnabled(true);
+            btnEdit.setEnabled(true);
+            txtShiftName.setText(arrShift.get(index).getShiftName());
+            String[] timetIn = arrShift.get(index).getTimeIn().split(":");
+            cbbTimeInH.setSelectedItem(timetIn[0]);
+            cbbTimeInM.setSelectedItem(timetIn[1]);
+            String[] timetOut = arrShift.get(index).getTimeOut().split(":");
+            cbbTimeOutH.setSelectedItem(timetOut[0]);
+            cbbTimeOutM.setSelectedItem(timetOut[1]);
+        }
+    }//GEN-LAST:event_tblShiftMouseClicked
+
+    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
+        // TODO add your handling code here:
+        disableFields();
+    }//GEN-LAST:event_btnCancelActionPerformed
+
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        // TODO add your handling code here:
+        selection = 1;
+        clearFields();
+        enableFields();
+    }//GEN-LAST:event_btnAddActionPerformed
+
+    private void btnEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditActionPerformed
+        // TODO add your handling code here:
+        selection = 0;
+        enableFields();
+    }//GEN-LAST:event_btnEditActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        // TODO add your handling code here:
+        selection = -1;
+        index = tblShift.getSelectedRow();
+        Shift shift = new Shift();
+        shift = arrShift.get(index);
+        int i = JOptionPane.showConfirmDialog(this, "Are you sure want to delete all data related to shift have shift name = " + shift.getShiftName());
+        if (i == JOptionPane.YES_OPTION) {
+            shiftSer = new ShiftServices();
+            if (shiftSer.remove(shift.getShiftID())) {
+                JOptionPane.showMessageDialog(this, shiftSer.getLastError(), "Message", JOptionPane.INFORMATION_MESSAGE);
+                loadDataOnTable();
+                clearFields();
+                disableFields();
+            } else {
+                JOptionPane.showMessageDialog(this, shiftSer.getLastError(), "Warning", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
+    private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
+        // TODO add your handling code here:
+        Shift shift = new Shift();
+        if (selection == 0) {
+            index = tblShift.getSelectedRow();
+            shift = arrShift.get(index);
+        }
+        shift.setShiftName(txtShiftName.getText().trim());
+
+        // get time in and time out
+        String timeIn = (String) cbbTimeInH.getSelectedItem();
+        timeIn += ":";
+        timeIn += cbbTimeInM.getSelectedItem();
+        String timeOut = (String) cbbTimeOutH.getSelectedItem();
+        timeOut += ":";
+        timeOut += cbbTimeOutM.getSelectedItem();
+
+        shift.setTimeIn(timeIn);
+        shift.setTimeOut(timeOut);
+
+        shiftSer = new ShiftServices();
+        if (selection == 1) {
+            if (shiftSer.create(shift)) {
+                JOptionPane.showMessageDialog(this, shiftSer.getLastError(), "Message", JOptionPane.INFORMATION_MESSAGE);
+                loadDataOnTable();
+                disableFields();
+            } else {
+                JOptionPane.showMessageDialog(this, shiftSer.getLastError(), "Warning", JOptionPane.WARNING_MESSAGE);
+            }
+        } else {
+            if (selection == 0) {
+                if (shiftSer.store(shift)) {
+                    JOptionPane.showMessageDialog(this, shiftSer.getLastError(), "Message", JOptionPane.INFORMATION_MESSAGE);
+                    loadDataOnTable();
+                    disableFields();
+                } else {
+                    JOptionPane.showMessageDialog(this, shiftSer.getLastError(), "Warning", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        }
+    }//GEN-LAST:event_btnSaveActionPerformed
+
     /**
-    * @param args the command line arguments
-    */
+     * load data on tablse
+     */
+    public void loadDataOnTable() {
+        shiftSer = new ShiftServices();
+        arrShift = shiftSer.findByAll();
+
+        ColumnData[] columns = {
+            new ColumnData("Shift name", 100, SwingConstants.LEFT, 1),
+            new ColumnData("Time in", 50, SwingConstants.LEFT, 2),
+            new ColumnData("Time out", 50, SwingConstants.LEFT, 3)
+        };
+        tableModel = new ObjectTableModel(tblShift, columns, arrShift);
+        headerTable = tableModel.getHeaderTable();
+
+        //tao cot stt tu dong
+        headerTable.createDefaultColumnsFromModel();
+
+        tblShift.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        // Put it in a viewport that we can control a bit
+        JViewport viewport = new JViewport();
+
+        //hien thi du lieu cot stt
+        viewport.setView(headerTable);
+
+        viewport.setPreferredSize(headerTable.getMaximumSize());
+
+        scrShift.setRowHeader(viewport);
+        scrShift.setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, headerTable.getTableHeader());
+    }
+
+    /**
+     * enable fields
+     */
+    public void enableFields() {
+        txtShiftName.setEditable(true);
+        cbbTimeInH.setEnabled(true);
+        cbbTimeInM.setEnabled(true);
+        cbbTimeOutH.setEnabled(true);
+        cbbTimeOutM.setEnabled(true);
+        btnAdd.setEnabled(true);
+        btnCancel.setEnabled(true);
+        btnDelete.setEnabled(false);
+        btnEdit.setEnabled(false);
+        btnSave.setEnabled(true);
+    }
+
+    /**
+     * disable fields
+     */
+    public void disableFields() {
+        clearFields();
+        txtShiftName.setEditable(false);
+        cbbTimeInH.setEnabled(false);
+        cbbTimeInM.setEnabled(false);
+        cbbTimeOutH.setEnabled(false);
+        cbbTimeOutM.setEnabled(false);
+        btnAdd.setEnabled(true);
+        btnCancel.setEnabled(false);
+        btnDelete.setEnabled(false);
+        btnEdit.setEnabled(false);
+        btnSave.setEnabled(false);
+    }
+
+    /**
+     * clear fields
+     */
+    public void clearFields() {
+        txtShiftName.setText("");
+        cbbTimeInH.setSelectedIndex(0);
+        cbbTimeInM.setSelectedIndex(0);
+        cbbTimeOutH.setSelectedIndex(0);
+        cbbTimeOutM.setSelectedIndex(0);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
+
             public void run() {
                 ShiftManagementDlg dialog = new ShiftManagementDlg(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
                         System.exit(0);
@@ -289,7 +517,6 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
             }
         });
     }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdd;
     private javax.swing.JButton btnCancel;
@@ -311,9 +538,8 @@ public class ShiftManagementDlg extends javax.swing.JDialog {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JScrollPane scrShift;
+    private javax.swing.JTable tblShift;
     private javax.swing.JTextField txtShiftName;
     // End of variables declaration//GEN-END:variables
-
 }
